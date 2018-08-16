eaf5bbce37e80c1229431a175c7baed5c21090b0
Allan Luartes <38699378+lanluartes@users.noreply.github.com>
2018-08-17 04:16:21 +0800

    redesigned.
    ikaw na bahala.
    
    naka bootstrap nalang yan.
    
    Co-Authored-By: mousynill <mousynill@users.noreply.github.com>

--- editcomics.php
+++ editcomics.php
@@ -64,18 +64,6 @@
                             <!-- modal body-->
                           <div class='modal-body'>
 
-                              <ul class="nav nav-tabs" id="tabsFor<?php echo $comicsID; ?>" role="tablist">
-                                <li class="nav-item">
-                                  <a class="nav-link active" id="SeriesDetails" data-toggle="tab" href="#SeriesDetailsFor<?php echo $comicsID; ?>" role= "tab" aria-controls="home" aria-selected="true">Series</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" id="UploadChapter" data-toggle="tab" href="#UploadChapterFor<?php echo $comicsID; ?>" role="tab" aria-controls="profile" aria-selected="false">Upload Chapter</a>
-                                </li>
-                              </ul>
-
-                              <div class="tab-content" id="myTabContent">
-                                <div class="tab-pane fade show active" id="SeriesDetailsFor<?php echo $comicsID; ?>" role="tabpanel" aria-labelledby="SeriesDetails">
-
                                     <div class='first-column'>
                                       <form id='form.<?php echo $comicsID; ?>' action='editcomicsfunction.php' method='POST' class='up' enctype='multipart/form-data'>
                                         Title: <input type='text' name='title' value=<?php echo $comicsTitle; ?>><br><br>
@@ -83,42 +71,75 @@
                                         Description:<br><textarea name='desc'rows='8' cols='36'><?php echo $comicsDesc; ?></textarea>
                                       </form>
                                       <!-- second column of the modal   -->
-                                        <div class='second-column'>
-                                          <!-- row container of the second column of the modal -->
-                                          <?php
-                                          $getChapterQuery = "SELECT * FROM trychaptertable WHERE seriesID = $comicsID";
-                                          if($getChapter = mysqli_query($conn, $getChapterQuery)){
-                                            while($getChapterRow = mysqli_fetch_row($getChapter)){
-                                              $seriesNo = $getChapterRow[0];
-                                              $chapterNo = $getChapterRow[1];
-                                              $chapterTitle = $getChapterRow[2];
-                                              $chapterPath = $getChapterRow[3];
-                                              ?>
-
-                                              <div class="table-row">
-                                                <button type="button" name="<?php echo $chapterNo; ?> "> <?php echo "Chapter $chapterNo: $chapterTitle"?> </button>
-                                                <button type="button" name="deleteBtn" id="delete" onclick="return validation()">X</button>
-                                              </div>
-
-                                            <?php };
-                                          }; ?>
-
-                                        </div>
                                     </div>
 
-                                </div>
+                                    <div class='second-column'>
 
-                                <div class="tab-pane fade" id="UploadChapterFor<?php echo $comicsID; ?>" role="tabpanel" aria-labelledby="UploadChapter">
+                                      <div class="container">
+                                        <div class="row">
+
+                                          <!-- row container of the second column of the modal -->
+                                          <?php
+                                            $getChapterQuery = "SELECT * FROM trychaptertable WHERE seriesID = $comicsID";
+                                            if($getChapter = mysqli_query($conn, $getChapterQuery)){
+                                              while($getChapterRow = mysqli_fetch_row($getChapter)){
+                                                $seriesNo = $getChapterRow[0];
+                                                $chapterNo = $getChapterRow[1];
+                                                $chapterTitle = $getChapterRow[2];
+                                                $chapterPath = $getChapterRow[3];
+                                          ?>
+
+                                                <div class="col-md-4" style="justify-content: space-between;">
+                                                    <a name="<?php echo $chapterNo; ?>" href="">
+                                                        <div class="myrowbutton">
+
+                                                            <text>
+                                                              <?php echo "Chapter $chapterNo:"; ?>
+                                                              <br>
+                                                              <?php echo "$chapterTitle"; ?>
+                                                            </text>
+
+                                                        </div>
+                                                    </a>
+                                                </div>
+
+                                            <?php };
+                                          };?>
+                                        </div>
+
+                                          <div class='container'>
+                                            <div class='row-reverse'>
+                                                <div class="col-12">
+                                                  <a href="#">
+                                                      <div class="myrowbutton" style="font-size: 80%; border-style: dashed; border-color: dodgerBlue;">
+                                                        +ADD A NEW CHAPTER
+                                                      </div>
+                                                  </a>
+                                                </div>
+                                              </div>
+                                          </div>
+
+                                      </div>
+
+                                    </div>
+                                  </div>
+
+
+
+                              <!-- this modal should be attached to the bottom of the main Modal/ pag clinick yung add new chapter, mag pop up to from the bottom.-->
+
+                                <!-- <div class="tab-pane fade" id="UploadChapterFor<?php echo $comicsID; ?>" role="tabpanel" aria-labelledby="UploadChapter">
                                   <form id="formuploadcomics.<?php echo $comicsID; ?>" class="formuploadcomics" action="addnewchapter.php" method='POST' class='up' enctype='multipart/form-data'>
                                       <input type="file" name="chapterFile" form="formuploadcomics.<?php echo $comicsID; ?>">
                                       <input type="hidden" name="whatthe" value="php echo $comicsID ?>">
                                       Chapter No: <input type='text' name="chapterNo"><br><br>
                                       Title: <br><input type='text' name="chapterTitle">
                                   </form>
-                                </div>
-                              </div>
+                                </div> -->
 
-                          </div>
+                                <!-- tapos kailangan pa ng isa pang modal pre, dun naman natin lalagay kung mag eedit ng chapter. -->
+
+                          <!-- </div> -->
 
                             <!-- this is the footer -->
                         <div class='modal-footer'>
@@ -164,6 +185,10 @@
       document.getElementById(container).style.display="none";
 
   }
+  $('#changetabbutton').click(function(e){
+    e.preventDefault();
+    $('#mytabs a[href="#second"]').tab('show');
+})
   </script>
 
   <!--script for deleting chapters-->
@@ -179,5 +204,32 @@
   }
   </script>
 
+  <style>
+
+      .myrowbutton{
+          border-radius: 2px;
+          cursor: pointer;
+          line-height: 1.8;
+          color: black;
+          padding: 12px;
+          margin-bottom: 12px;
+          border: solid 1px #000;
+          transition: 0.05s;
+          font-size: 68%;
+
+
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+      }
+
+      a, a:hover {
+        text-decoration: none;
+
+      }
+
+
+
+  </style>
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -58,4 +58,5 @@ $app->get('/api/{path}', function (Request $request, Response $response, array $
 });
 
 
+
 $app->run();
