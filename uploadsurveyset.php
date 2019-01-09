2ed8802edd6aa9fcd6e5124254c58eb87f9b74fa
lanluartes <lan.luartes@gmail.com>
2019-01-09 08:39:50 +0800

    Achieved the desired design.

--- uploadsurveyset.php
+++ uploadsurveyset.php
@@ -7,20 +7,21 @@
   <link rel="stylesheet" type="text/css" href="uploadsurveyset.css">
   <link href="https://fonts.googleapis.com/css?family=Quicksand:500" rel="stylesheet">
 
+<div class="container-fluid flexmine">
 
-  <div class="row flexmine">
+  <div class="row card">
 
-      <div class="col-md-6 col-sm-12 flexmine">
-          <button>Upload by xlsx</button>
+      <div class="choice-container">
+          <button class="rebutton rebutton-left" type="button" id="a">Upload by xlsx</button>
       </div>
 
 
 
-      <div class="col-md-6 col-sm-12 flexmine">
-          <button>Make a set here</button>
+      <div class="choice-container">
+          <button class="rebutton rebutton-right" type="button" id="b">Make a set here</button>
       </div>
 
 
   </div>
-
+</div>
 </body>
